d37f2ae6233777b6ed7b25a4d7d184f88e173c3e
raliwalas <rachel.aliwalas52@gmail.com>
2023-10-16 20:02:29 -0400

    continuing with GoRails tutorials - this is part 4

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,7 @@
+# without this file, you will get a routing error
+
+# inherit from application controller aka action controller
+# gives us the info we need to process HTTP requests like parameters
+    # and appropriate responses
+class BlogPostController < ApplicationController 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #index action will display all of the blog posts
+   root "blog_posts#index" 
 end
