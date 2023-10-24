552e2ae2751de89df44ef914bf502466e589aa35
raliwalas <rachel.aliwalas52@gmail.com>
2023-10-24 06:39:15 -0400

    comment and tailwindcss gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # find or create the account with this email address
+# remade my account in console - not sure if this 100% works where it creates account
+#  if not found
+# fixme for later
 user = User.where(email: "rachel.aliwalas52@gmail.com").first_or_initialize
 
 #! allows us to run and rerun without errors
