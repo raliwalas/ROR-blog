0d2fe9ec1f44d59ea69416a9459c4bd4659aad84
raliwalas <rachel.aliwalas52@gmail.com>
2023-10-24 06:31:52 -0400

    fixing syntax part 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # find or create the account with this email address
-# maybe revert or try again with yale email
 user = User.where(email: "rachel.aliwalas52@gmail.com").first_or_initialize
 
 #! allows us to run and rerun without errors
